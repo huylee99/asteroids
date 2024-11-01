38eadd63f2233e64db4ca59d9e5184f09daec999
huylee99 <nhathuyle083@outlook.com>
2024-10-31 22:00:14 -0700

    add player

--- circleshape.py
+++ circleshape.py
@@ -1,23 +1,22 @@
 import pygame
 
 class CircleShape(pygame.sprite.Sprite):
-    def __init__(self, x, y, radius):
-        def __init__(self, x, y, radius):
+  def __init__(self, x, y, radius):
         # we will be using this later
-          if hasattr(self, "containers"):
-              super().__init__(self.containers)
-          else:
-              super().__init__()
+    if hasattr(self, "containers"):
+      super().__init__(self.containers)
+    else:
+      super().__init__()
 
-        self.position = pygame.Vector2(x, y)
-        self.velocity = pygame.Vector2(0, 0)
-        self.radius = radius
+    self.position = pygame.Vector2(x, y)
+    self.velocity = pygame.Vector2(0, 0)
+    self.radius = radius
 
-    def draw(self, screen):
+  def draw(self, screen):
         # sub-classes must override
-        pass
+    pass
 
-    def update(self, dt):
+  def update(self, dt):
         # sub-classes must override
-        pass
+    pass
 
--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import pygame
-from constants import *
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
 
 def main():
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   clock = pygame.time.Clock()
+  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
   dt = 0
-
-
-
-
 
   while True:
     for event in pygame.event.get():
@@ -17,10 +15,13 @@ def main():
         return
       
     pygame.Surface.fill(screen, "black")
+    player.draw(screen)
     pygame.display.flip()
     tick = clock.tick(60)
     dt = tick / 1000
     
+  
+  
 
 if __name__ == "__main__":
   main()
